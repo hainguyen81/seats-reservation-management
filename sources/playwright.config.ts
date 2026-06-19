e3b9628b950e63f9952337e054dcc484c1fb953a
hainguyen81 <hainguyen81@users.noreply.github.com>
2026-06-19 11:33:55 +0700

    - config playwright to test with postgres on CI/CD

--- sources/playwright.config.ts
+++ sources/playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
 
     use: {
         // test URL
-        baseURL: 'http://localhost:3000',
+        baseURL: 'http://127.0.0.1:3000',
         trace: 'on-first-retry', // trace test error on first retry time
         screenshot: 'only-on-failure', // capture test failed to debug
         video: 'retain-on-failure', // record video if failure
@@ -20,9 +20,21 @@ export default defineConfig({
     /* 💡 Package server: auto start/stop application */
     webServer: {
         command: 'npm run dev',
-        url: 'http://localhost:3000',
+        url: 'http://127.0.0.1:3000',
         reuseExistingServer: !process.env.CI,
         timeout: 120 * 1000,
+        env: {
+            DATABASE_PROVIDER: "postgres",
+            AUTH_PROVIDER: "custom",
+            NEXT_PUBLIC_AUTH_PROVIDER: "custom",
+            DATABASE_URL: "postgresql://postgres:postgres_secret_pass@localhost:5432/seats_reservation?schema=public&pgbouncer=true",
+            DIRECT_DATABASE_URL: "postgresql://postgres:postgres_secret_pass@localhost:5432/seats_reservation?schema=public",
+            REDIS_URL: "redis://redis-cache:6379",
+            JWT_SECRET: "linkz-assessment-senior-lead-secret-key-2026",
+            TOTAL_SEATS: "15",
+            SHOULD_SEED: "true",
+            NODE_ENV: 'test'
+        }
     },
 
     projects: [
