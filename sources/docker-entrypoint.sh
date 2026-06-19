4b1e770979dd207854080fc0221e9863132a431b
hainguyen81 <hainguyen81@users.noreply.github.com>
2026-06-19 15:11:14 +0700

    - fix docker

--- sources/docker-entrypoint.sh
+++ sources/docker-entrypoint.sh
@@ -14,12 +14,9 @@ if [ ! -f prisma/schema.prisma ]; then
   fi
 fi
 
-# Check if prisma doesn't exist, install present project version 5.10.0
-echo "📊 Running Prisma DB Push..."
-[ ! -f /app/node_modules/.bin/prisma ] && npx prisma@5.10.0 db push
 # Use pre-installed prisma binary from node_modules
 # This prevents npx from auto-installing latest version
-[ -f /app/node_modules/.bin/prisma ] && /app/node_modules/.bin/prisma db push
+/app/node_modules/.bin/prisma db push
 
 if [ "$SHOULD_SEED" = "true" ]; then
   echo "🚀 Seeding sample data..."
