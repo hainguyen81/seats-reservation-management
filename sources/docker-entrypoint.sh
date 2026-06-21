da169d1078cad81688667eead9e1ff46c6ba5ad2
hainguyen81 <hainguyen81@users.noreply.github.com>
2026-06-21 21:38:50 +0700

    - fix for deploying K8s

--- sources/docker-entrypoint.sh
+++ sources/docker-entrypoint.sh
@@ -10,25 +10,6 @@ if [ "$DATABASE_PROVIDER" = "postgres" ]; then
 	[ -f prisma/schema.postgres.prisma ] && cp prisma/schema.postgres.prisma prisma/schema.prisma && cp prisma/schema.postgres.prisma prisma/dist/schema.prisma || echo "⚠️  schema.postgres.prisma not found"
 else
 	[ -f prisma/schema.sqlite.prisma ] && cp prisma/schema.sqlite.prisma prisma/schema.prisma && cp prisma/schema.sqlite.prisma prisma/dist/schema.prisma || echo "⚠️  schema.sqlite.prisma not found"
-
-  # check to create SQLite db file if not found
-  if [ -n "$DATABASE_URL" ]; then
-    # sed `file:` prefix
-    DB_PATH=$(echo "$DATABASE_URL" | sed 's/^file://')
-    
-    # parse parent folder path
-    DB_DIR=$(dirname "$DB_PATH")
-    
-    # create folder if not found
-    mkdir -p "$DB_DIR"
-    
-    # check file whether existed
-    if [ ! -f "$DB_PATH" ]; then
-      touch "$DB_PATH"
-      chmod 666 "$DB_PATH"
-      echo "✅ Created file DB successful from env: $DB_PATH"
-    fi
-  fi
 fi
 
 if [ "$SHOULD_SEED" = "true" ]; then
