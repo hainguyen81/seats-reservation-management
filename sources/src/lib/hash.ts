12620107d0eedd1ec2e86cb571f6d00b0b60b4e5
hainguyen81 <hainguyen81@users.noreply.github.com>
2026-06-30 15:54:36 +0700

    - refactor code

--- sources/src/lib/hash.ts
+++ sources/src/lib/hash.ts
@@ -1,18 +1,18 @@
 import { hashSync, compareSync } from 'bcrypt-ts';
 
-// 💡 Tham số tạo độ phức tạp thuật toán (Tiêu chuẩn an toàn bảo mật)
+// 💡 salt to hash
 const SALT_ROUNDS = 10;
 
 /**
- * Hàm mã hóa mật khẩu thô sang chuỗi Hash bằng thuật toán tịnh tiến thời gian
+ * Hash password
  */
 export function hashPassword(password: string): string {
     return hashSync(password, SALT_ROUNDS);
 }
 
 /**
- * Hàm giải mã và so khớp mật khẩu thô với chuỗi mật khẩu đã mã hóa trong DB
- * @returns true nếu chính xác, false nếu sai mật khẩu
+ * Compare hashed password
+ * @returns true/false
  */
 export function comparePassword(password: string, hash: string): boolean {
     return compareSync(password, hash);
