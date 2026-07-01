72fbecf88ad180ff3b15b6ef0d0cee0bdd95abec
hainguyen81 <hainguyen81@users.noreply.github.com>
2026-07-01 23:23:38 +0700

    - update config

--- sources/next.config.mjs
+++ sources/next.config.mjs
@@ -9,7 +9,7 @@ const standalone = process.env.BUILD_STANDALONE;;
 const productionMode = nodeEnv === "production";
 const mobileMode = ["mobile", "android", "ios"].includes(nodeEnv);
 const buildStandalone = standalone === "true";
-console.warn(`Next.js: PRODUCTION Mode ?. ${productionMode} - MOBILE (APK/iOS) Mode?. ${mobileMode}`);
+console.warn(`Next.js: PRODUCTION Mode ?. ${productionMode} - MOBILE (APK/iOS) Mode?. ${mobileMode ? "true" : "false"}`);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = mobileMode
