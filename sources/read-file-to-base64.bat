3be3c534d5d1420c99e3af52ca4646791a5e1009
hainguyen81 <hainguyen81@users.noreply.github.com>
2026-07-02 10:47:23 +0700

    - add script to read .keystore/.jks file to base64 to input into GitHub Secrets

---
+++ sources/read-file-to-base64.bat
@@ -0,0 +1,34 @@
+@echo off
+setlocal enabledelayedexpansion
+chcp 65001 > nul
+
+SET "TAB=	"
+SET "INPUT=%~1"
+SET "OUTPUT=%~2"
+
+:: Check if the file exists
+if not exist "%INPUT%" (
+    echo %TAB%- ❌ Error: %INPUT% not found.
+    exit /b
+)
+if /I "%OUTPUT%"=="" (
+	for %%I in ("%INPUT%") do (
+        :: parse full file name including extension
+        set "OUTPUT=%%~nxI"
+        echo FULL NAMEEEEEEEEEEEEEEEE !OUTPUT!
+        
+        :: parse only base file name
+        for %%A in ("!OUTPUT!") do set "OUTPUT=%%~nA"
+        echo FILE NAMEEEEEEEEEEEEEEEE !OUTPUT!
+        
+		:: build output file name
+        set "OUTPUT=!OUTPUT!.base64.txt"
+    )
+)
+
+:: Encode the file to Base64
+echo %TAB%- ⚡ Read file %INPUT% to base64 file %OUTPUT%
+certutil -encode "%INPUT%" "%OUTPUT%" >nul
+echo %TAB%---^> 🎉 Successfully! Base64 Output File: %OUTPUT%
+
+pause
